54c3220ba59077ccf663ff822d580318d7fd2e40
callmehaider <thehaiderplays@gmail.com>
2024-10-28 12:32:01 +0500

    Lab 3 tasks

---
+++ LAb 3 tasks.cpp
@@ -0,0 +1,70 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	//task 9
+	cout << "  *"<<endl;
+	cout << "*****"<<endl;
+	cout << "  *" << endl;
+	//task 10
+	cout << "***" << endl;
+	cout << "* *" << endl;
+	cout << "***" << endl;
+	//task 11
+	cout << " ***" <<endl;
+	cout << "*****" <<endl;
+	cout << "  *" <<endl;
+	cout << "  *" <<endl;
+	//task 12
+	cout << "********************************" << endl;
+	cout << "*    Programming Assignment 1  *" << endl;
+	cout << "*     Computer Programming I   *" << endl;
+	cout << "*      Author :Haider Ali      *" << endl;
+	cout << "* Due Date : Thursday,Jan.24   *" << endl;
+	cout << "********************************" << endl;
+	//task 13
+	cout << "cccccccccc     ++               ++" << endl;
+	cout << "cc             ++               ++" << endl;
+	cout << "cc       ++++++++++++++   ++++++++++++++" << endl;
+	cout << "cc       ++++++++++++++   ++++++++++++++" << endl;
+	cout << "cc             ++               ++" << endl;
+	cout << "cccccccccc     ++               ++" << endl;
+	//task 14
+	cout << " 2x1 = 2 " << endl;
+	cout << " 2x2 = 4 " << endl;
+	cout << " 2x3 = 6 " << endl;
+	cout << " 2x4 = 8 " << endl;
+	cout << " 2x5 = 10" << endl;
+	cout << " 2x6 = 12" << endl;
+	cout << " 2x7 = 14" << endl;
+	cout << " 2x8 = 16" << endl;
+	cout << " 2x9 = 18" << endl;
+	cout << " 2x10= 20" << endl;
+	//task 15
+	cout << "\t\t\t ************" << endl; 
+	cout << "\t\t\t Result Sheet" << endl;
+	cout << "\t\t\t ************" << endl;
+	cout << "Student Roll no.\tMath\tPhysics   Chemistry\tTotal" << endl;
+	cout << "---------------------------------------------------------------" << endl;
+	cout << "  2357          \t 99 \t  85         97   \t  281" << endl;
+	cout << "---------------------------------------------------------------" << endl;
+	//task 16
+	cout <<25/3  << endl;
+	cout << 20 - 12 / 4 * 2 << endl;
+	cout << 32 %7 << endl;
+	cout << 3-5 % 7 << endl;
+	cout << 18.0/4 << endl;
+	cout << 28-5 /2.0 << endl;
+	cout << 17 + 5 %2-3<< endl;
+	cout << 15.0 + 3.0 * 2.0 / 5.0 << endl;
+	//task 18
+	cout <<"** **"<< endl;
+	cout << "*****" << endl;
+	cout << "*****" << endl;
+	cout << " *** " << endl;
+	cout << "  *  " << endl;
+
+
+	return 0;
+
+}
